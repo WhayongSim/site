9119c9e6d9d51ee5c6bc072c490662026fe6108c
WhayongSim <114666808+WhayongSim@users.noreply.github.com>
2023-06-25 01:32:59 +0900

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set("port", process.env.PORT || 8000);
 
-
+app.use(express.static(path.join(__dirname, '/build')));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
