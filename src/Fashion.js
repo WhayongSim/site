d5df246747724598c225d4baebdb282eb2721081
WhayongSim <114666808+WhayongSim@users.noreply.github.com>
2023-07-21 00:05:16 +0900

    Update Fashion.js

--- src/Fashion.js
+++ src/Fashion.js
@@ -92,7 +92,7 @@ const IMGLIST = ['/fashion/menswear/01.jpg', '/fashion/menswear/02.jpg', '/fashi
   '/fashion/menswear/07.jpg', '/fashion/menswear/08.jpg', '/fashion/menswear/09.jpg',
   '/fashion/menswear/10.jpg', '/fashion/menswear/11.jpg', '/fashion/menswear/12.jpg', '/fashion/menswear/13.jpg',
   '/fashion/menswear/14.jpg', '/fashion/menswear/15.jpg', '/fashion/menswear/16.jpg', '/fashion/menswear/17.jpg',
-  '/fashion/menswear/18.jpg'
+  '/fashion/menswear/18.jpg', '/slovenmode/IMG_6123.JPG',  '/slovenmode/IMG_6124.JPG', '/slovenmode/IMG_6125.JPG'
 ];
 
   const openImageViewer = useCallback((index) => {
