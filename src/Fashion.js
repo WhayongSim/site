066692d46d30684736a95ee7155b7c659f5f4cd0
WhayongSim <114666808+WhayongSim@users.noreply.github.com>
2023-07-20 23:22:41 +0900

    Update Fashion.js

--- src/Fashion.js
+++ src/Fashion.js
@@ -41,7 +41,23 @@ function FashionMain({ open, openMenu, closeMenu }) {
         <FullPage>
          <Slide>
              <Link to="/fashion/menswear" style={{ width: '50%', textAlign: 'center' }}>
-              <img src="/fashion/fashion-menswear.jpg" width="80%" />
+             <YouTube
+              //videoId : https://www.youtube.com/watch?v={videoId} 유튜브 링크의 끝부분에 있는 고유한 아이디
+                videoId={"f2GEpw8fxjs"}
+              //opts(옵션들): 플레이어의 크기나 다양한 플레이어 매개 변수를 사용할 수 있음.
+              //밑에서 더 설명하겠습니다.
+                opts={{
+                  width: "100%",
+                  height: "100%",
+                  playerVars: {
+                    autoplay: 1, //자동재생 O
+                    rel: 0, //관련 동영상 표시하지 않음 (근데 별로 쓸모 없는듯..)
+                    modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
+                  },
+                }}
+                //이벤트 리스너 
+                onEnd={(e)=>{e.target.stopVideo(0);}}      
+              />
             </Link>
           </Slide>
           <Slide>
