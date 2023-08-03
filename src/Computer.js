248190f3a744a05cc39a82e3def6c6091f01f4d7
WhayongSim <114666808+WhayongSim@users.noreply.github.com>
2023-08-03 21:42:30 +0900

    Update Computer.js

--- src/Computer.js
+++ src/Computer.js
@@ -82,17 +82,20 @@ function Computer({ open, openMenu, closeMenu }) {
                     fontWeight: "bold",
                     marginBottom: 15,
                     marginLeft: 28,
+                    paddingRight: 15,
                   }}
                 >
-                  Hacking
+                  Hacking & Security
                 </div>
                 <div style={{ fontSize: 20, lineHeight: 2, marginLeft: 28 }}>
-                  고등학생 때 교내 해킹 대회 입상을 계기로 해킹과 보안 기술에
-                  관심을 갖게 되어, 각종 해킹 방어 대회에 참가했습니다. 그중
-                  해커들의 올림픽이라고 불리는 DEF CON CTF에 'HackingForChiMac'
-                  팀으로 참가해 본선에 진출하는 쾌거를 이뤘으며, 라스베가스에
-                  직접 방문하여, 실력이 출중한 해커들과 교류하는 시간을
-                  가졌습니다.
+                  When I was a high school student, I became interested in
+                  hacking and security technologies after be awarded the school
+                  hacking defense competition, and I participated in various
+                  hacking competitions while learning various hacking
+                  technologies. In the end, I participated in the DEF CON 22
+                  CTF, the world's authoritative hacking defense competition
+                  held every summer in Las Vegas, USA, with the HackingForChiMac
+                  team, and made it to the finals.
                 </div>
               </div>
             </div>
@@ -122,21 +125,27 @@ function Computer({ open, openMenu, closeMenu }) {
               </div>
               <div style={{ marginLeft: 10 }}>
                 <div
-                  style={{ fontSize: 30, fontWeight: "bold", marginBottom: 15 }}
+                  style={{
+                    fontSize: 30,
+                    fontWeight: "bold",
+                    marginBottom: 15,
+                    paddingRight: 15,
+                  }}
                 >
-                  Development
+                  Development & Network
                 </div>
                 <div style={{ fontSize: 20, lineHeight: 2 }}>
-                  위게임 사이트는 해킹 기술과 정보 보안 역량을 테스트하고 향상
-                  시키기 위해 설계된 웹사이트로, 다양한 해킹 과제를 제공하여
-                  실제 시스템에 피해를 주지 않고 해킹 기술을 연습할 수 있습니다.
-                  국제 워게임 사이트 '핵미'에서 문제 출제와 웹 개발 및 서버 유지
-                  보수를 담당하며 사이트 운영에 참여했습니다.
+                  Wargame sites are websites designed to improve hacking skills
+                  and information security competencies, offering a variety of
+                  hacking challenges to practice your hacking skills without
+                  harming real systems. As an operator of the international
+                  wargame site Hack-Me, I was in charge of posting challenges,
+                  web development and server management, and I've organized and
+                  hosted security conferences.
                 </div>
               </div>
             </div>
           </Slide>
-          
         </FullPage>
       </div>
     </div>
