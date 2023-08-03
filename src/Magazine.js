d41a86dc343c96e11e1eeea8eee147bc21d70a5e
WhayongSim <114666808+WhayongSim@users.noreply.github.com>
2023-08-04 00:03:07 +0900

    Update Magazine.js

--- src/Magazine.js
+++ src/Magazine.js
@@ -1,47 +1,77 @@
 function Magazine({ open, openMenu, closeMenu, data }) {
-    console.log(data);
-    return (
-      <div className="pageRoot">
-        <div className="header">
-          <div className="flex">
-            <img src="/main/magazine.png" alt="" width={50} />
-            <span>Magazine</span>
-          </div>
-          <div className="menuRoot">
-            {open && (
-              <div
-                className="menWrap"
-                style={{ cursor: 'pointer', background: '#ddd' }}
-                onClick={closeMenu}
-              >
-                <span>Menu</span>
-                <img src="/menu/menu-open.png" alt="" width={50} style={{ marginLeft: 15 }} />
-              </div>
-            )}
-            {!open && (
-              <div className="menWrap" style={{ cursor: 'pointer' }} onClick={openMenu}>
-                <span>Menu</span>
-                <img src="/menu/menu-close.png" alt="" width={50} style={{ marginLeft: 15 }} />
-              </div>
-            )}
-          </div>
+  console.log(data);
+  return (
+    <div className="pageRoot">
+      <div
+        className="header"
+        style={{
+          position: "fixed",
+          top: "0px",
+          left: "0px",
+          width: "100%",
+          paddingLeft: "0px",
+          right: "0px",
+          zIndex: "10",
+        }}
+      >
+        <div className="flex" style={{ paddingLeft: 50 }}>
+          <img src="/main/magazine.png" alt="" width={50} />
+          <span>Magazine</span>
         </div>
-        <div className="innerRoot" style={{background: '#ddd'}}>
-          <div className="cardWrap">
-{data.map((it)=>(
-              <a className="card" href={it.url} target="_blank">
-                <div className="cardTop" style={{backgroundImage: `url(${it.image_url})`}}></div>
-                <div className="cardBottom">
-                  <div className="cardTitle">{it.title}</div>
-                  <div className="cardDate">{it.content}</div>
-                </div>
-              </a>
-            ))}
-          </div>
+        <div className="menuRoot">
+          {open && (
+            <div
+              className="menWrap"
+              style={{ cursor: "pointer", background: "#ddd" }}
+              onClick={closeMenu}
+            >
+              <span>Menu</span>
+              <img
+                src="/menu/menu-open.png"
+                alt=""
+                width={50}
+                style={{ marginLeft: 15 }}
+              />
+            </div>
+          )}
+          {!open && (
+            <div
+              className="menWrap"
+              style={{ cursor: "pointer" }}
+              onClick={openMenu}
+            >
+              <span>Menu</span>
+              <img
+                src="/menu/menu-close.png"
+                alt=""
+                width={50}
+                style={{ marginLeft: 15 }}
+              />
+            </div>
+          )}
         </div>
       </div>
-    );
-  }
-  
-  export default Magazine;
-  
+      <div
+        className="innerRoot"
+        style={{ background: "#ddd", paddingTop: 100 }}
+      >
+        <div className="cardWrap">
+          {data.map((it) => (
+            <a className="card" href={it.url} target="_blank">
+              <div
+                className="cardTop"
+                style={{ backgroundImage: `url(${it.image_url})` }}
+              ></div>
+              <div className="cardBottom">
+                <div className="cardTitle">{it.title}</div>
+                <div className="cardDate">{it.content}</div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Magazine;
