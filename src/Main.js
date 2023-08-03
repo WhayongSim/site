dc941c761039b3b9b30a33e997aad5a88b7ef970
WhayongSim <114666808+WhayongSim@users.noreply.github.com>
2023-08-04 00:04:30 +0900

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <div style={{ width: '100%', textAlign: 'center' }} className="vertical-center">
+    <div
+      style={{ width: "100%", textAlign: "center" }}
+      className="vertical-center"
+    >
       <div>
-        <img src="/main/me.jpg" alt="" style={{ height: '550px' }} />
+        <Link to="/info">
+          <img src="/main/me.jpg" alt="" style={{ height: "550px" }} />
+        </Link>
       </div>
       <div
-        style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 10 }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 10,
+        }}
       >
         <Link to="/computer">
           <img src="/main/computer.png" alt="" width={90} />
-          <div style={{ fontWeight: 'bold', fontSize: 23 }}>Computer</div>
+          <div style={{ fontWeight: "bold", fontSize: 23 }}>Computer</div>
         </Link>
-        <Link to="/fashion" style={{ margin: '0px 100px' }}>
+        <Link to="/fashion" style={{ margin: "0px 100px" }}>
           <img src="/main/fashion.png" alt="" width={90} />
-          <div style={{ fontWeight: 'bold', fontSize: 23 }}>Fashion</div>
+          <div style={{ fontWeight: "bold", fontSize: 23 }}>Fashion</div>
         </Link>
         <Link to="/magazine">
           <img src="/main/magazine.png" alt="" width={90} />
-          <div style={{ fontWeight: 'bold', fontSize: 23 }}>Magazine</div>
+          <div style={{ fontWeight: "bold", fontSize: 23 }}>Magazine</div>
         </Link>
       </div>
     </div>
